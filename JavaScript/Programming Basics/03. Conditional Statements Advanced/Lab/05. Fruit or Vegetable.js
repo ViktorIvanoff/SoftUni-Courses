ef26cb97c54025f8f6c73079273e0527ad8488d0
ViktorIvanoff <viktorivanofs@gmail.com>
2022-02-24 14:23:05 +0200

    Conditional Statements Advanced

---
+++ JavaScript/Programming Basics/03. Conditional Statements Advanced/Lab/05. Fruit or Vegetable.js
@@ -0,0 +1,24 @@
+function solve(str) {
+
+    switch (str) {
+    
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+      console.log("fruit");
+      break;
+    case "tomato":
+    case "cucumber":
+    case "carrot":
+    case "pepper":
+     console.log("vegetable");
+     break;
+     default:
+         console.log("unknown");
+         break;
+    }
+    
+    }
