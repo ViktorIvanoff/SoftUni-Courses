327e6164bb8eebe930b549aab6c803afac8e6e16
Victor Ivanov <viktorivanofs@gmail.com>
2025-02-12 10:53:53 +0400

    QA
    QA

--- JavaScript/JavaScript Advanced/JavaScript Advanced Module/04. DOM Introduction/Exercise/01. Subtraction/subtract.js
+++
@@ -1,9 +0,0 @@
-function subtract() {
-    
-    let firstNum = document.getElementById('firstNumber').value;
-    let secondNum = document.getElementById('secondNumber').value;
-
-    let result = Number(firstNum) - Number(secondNum);
-    document.getElementById('result').textContent = result;
-
-}
