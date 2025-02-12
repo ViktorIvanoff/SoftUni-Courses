d4e2d9798ceb7a84f58db965f91a8ea2d4a9864d
Victor Ivanov <viktorivanofs@gmail.com>
2025-02-12 10:38:57 +0400

    QA
    QA

---
+++ JavaScript Web Development Courses/JavaScript Advanced/JavaScript Advanced Module/01. Syntax, Functions & Statements/Lab/Circle Area.js
@@ -0,0 +1,19 @@
+function solve(arg) {
+    let result = 0;
+
+    let inputType = typeof(arg);
+
+    if (inputType == 'number') {
+
+        result = Math.pow(arg, 2) * Math.PI;
+
+        console.log(result.toFixed(2));
+    } else {
+
+        console.log(`We can not calculate the circle area, because we receive a ${inputType}.`);
+
+    }
+}
+solve(5);
+
+solve('name');
