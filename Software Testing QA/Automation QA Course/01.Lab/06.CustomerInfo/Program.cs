8b83dfb3edb946d50925478203a8574556ace467
Victor Ivanov <viktorivanofs@gmail.com>
2025-05-15 13:46:13 +0400

    Automation QA
    Automation QA

---
+++ Software Testing QA/Automation QA Course/01.Lab/06.CustomerInfo/Program.cs
@@ -0,0 +1,11 @@
+﻿//•	Read two floating-point numbers: distance and time
+//•	Calculate the speed needed to travel the specified distance for the specified time: 
+//speed = distance / time
+//•	Print the calculated speed formatted to 2nd digit
+
+double distance = double.Parse(Console.ReadLine());
+double time = double.Parse(Console.ReadLine());
+double speed = distance / time; ;
+Console.WriteLine($"{speed:F2}");
+
+
---
+++ Software Testing QA/Automation QA Course/1.ConvertorUSDtoEUR/02.KeyConcepts-Lab/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Write a program to convert from USD to EUR:
+//•	Read a floating-point number: the dollars to be converted 
+//•	Convert dollars to euro (use fixed rate of dollars to euro: 0.88)
+//•	Print the converted value in euro formatted to the 2nd digit
+double dollars = double.Parse(Console.ReadLine());
+double dollarsToEur = dollars * 0.88;
+Console.WriteLine($"{dollarsToEur:F2}");
+
