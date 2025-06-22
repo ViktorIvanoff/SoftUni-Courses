c11d1a0a842354338aac506a8d339059465dc759
Victor Ivanov <viktorivanofs@gmail.com>
2025-06-22 18:11:14 +0400

    C# Methods - Exercise
    C# Methods - Exercise

---
+++ Software Testing QA/Automation QA Course/Methods - Exercise/03.MultiplicationSign/Program.cs
@@ -0,0 +1,25 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+string result = MultiplicationSign(num1, num2, num3);
+Console.WriteLine(result);
+
+static string MultiplicationSign(int num1, int num2, int num3)
+{
+    int multiplication = 0;
+    string solution;
+    multiplication = num1 * num2 * num3;
+    if (multiplication < 0)
+    {
+        solution = "negative";
+    }
+    else if (multiplication > 0) {
+        solution = "positive";
+    }
+    else
+        {
+            solution = "zero";
+        }
+    return solution;
+}
---
+++ Software Testing QA/Automation QA Course/Methods - Exercise/06.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,29 @@
+﻿int number = Math.Abs(int.Parse(Console.ReadLine()));
+int result = GetMultipleOfEvenAndOdds(number);
+Console.WriteLine(result);
+
+static int GetMultipleOfEvenAndOdds(int number)
+{
+    int evenSum = 0;
+    int oddSum = 0;
+    //conv numb to str so can iterate it
+    string intToStr = number.ToString();
+
+    int product = 0;
+
+    for (int i = 0; i < intToStr.Length; i++)
+    {
+        //convert the curr numb
+        int currentNum = intToStr[i] - '0';
+        if (currentNum % 2 == 0)
+        {
+            evenSum += currentNum;
+        }
+        else
+        {
+            oddSum += currentNum;
+        }
+    }
+    product = Math.Abs(evenSum * oddSum);
+    return product;
+}
