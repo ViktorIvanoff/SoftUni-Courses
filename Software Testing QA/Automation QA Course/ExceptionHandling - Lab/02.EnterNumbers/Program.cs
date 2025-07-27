729bf961fc65347c5228c4db7773a2e15c1fd7c6
Victor Ivanov <viktorivanofs@gmail.com>
2025-07-27 15:12:39 +0400

    Exception Handling - Lab
    Exception Handling - Lab

---
+++ Software Testing QA/Automation QA Course/ExceptionHandling - Lab/02.EnterNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿//Your number is not in range 1 - 100!
+//Your number is not in range 2 – 100!
+//Invalid Number!
+//2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+List<int> numbers = new List<int>();
+int start = 1;
+
+while(numbers.Count < 10)
+{
+	try
+	{
+        int currentNumber = ReadNumber(start, 100);
+        numbers.Add(currentNumber);
+        start = currentNumber;
+    }
+	catch (ArgumentOutOfRangeException)
+	{
+        Console.WriteLine($"Your number is not in range {start} - 100!");
+    }
+    catch(FormatException)
+    {
+        Console.WriteLine("Invalid Number!");
+    }
+}
+
+Console.WriteLine(string.Join(", ", numbers));
+
+int ReadNumber(int start, int end)
+{
+ 
+    int number = int.Parse(Console.ReadLine());
+    if (number <= start || number >= end)
+    {
+        throw new ArgumentOutOfRangeException();
+    }
+        return number;
+}
---
+++ Software Testing QA/Automation QA Course/ExceptionHandling - Lab/03.SumOfIntegers/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//2147483649 2 3.4 5 invalid 24 -4
+string[] elements = Console.ReadLine().Split();
+List<int> numbers = new List<int>();
+
+foreach (var element in elements)
+{
+
+    try
+    {
+        int number = int.Parse(element);
+        numbers.Add(number);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{element}' is in wrong format!");
+    }
+    catch(OverflowException)
+    {
+        Console.WriteLine($"The element '{element}' is out of range!");
+    }
+    finally
+    {
+        Console.WriteLine($"Element '{element}' processed - current sum: {numbers.Sum()}");
+    }
+}
+Console.WriteLine($"The total sum of all integers is: {numbers.Sum()}");
